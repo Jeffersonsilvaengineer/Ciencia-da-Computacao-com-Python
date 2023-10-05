abd9b70b4c2e64b032ce9a39f02247dfee884e4f
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-10-05 20:03:22 -0300

    Exercicios

---
+++ Exercicio 2.py
@@ -0,0 +1,21 @@
+'''Exercício 2
+Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética. Observe o exemplo abaixo:
+
+Entrada de Dados:
+
+Digite a primeira nota: 4
+Digite a segunda nota: 5
+Digite a terceira nota: 6
+Digite a quarta nota: 7
+
+Saída de Dados:
+
+A média aritmética é 5.5'''
+
+notas = []
+for c in range(4):
+    nota = notas.append(float(input(f'Digite a {c+1}º nota: ')))
+soma = sum(notas)
+media = soma / len(notas)
+print("A soma dos itens da lista é:", soma)
+print(f'Amédia aritimética é igual a {media}')
