d76ead37f13234c31dc3a2818727c1dae5599621
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-10-07 15:01:08 -0300

    Exercicios

---
+++ Exercicio 4.py
@@ -0,0 +1,13 @@
+'''Reescreva o programa contaSegundos para imprimir também a quantidade de dias, ou seja,
+faça um programa em Python que, dada a quantidade de segundos, "quebre" esse valor em dias, horas, minutos e segundos.
+A saída deve estar no formato: a dias, b horas, c minutos e d segundos. Seja cuidadoso com o formato! Espaços a mais,
+vírgulas faltando ou outras diferenças são considerados erro
+
+Abaixo um exemplo de como deve ser a entrada e saída de dados do programa:
+
+Entrada de Dados:
+Por favor, entre com o número de segundos que deseja converter: 178615
+
+Saída de Dados:
+2 dias, 1 horas, 36 minutos e 55 segundos.'''
+
---
+++ Exercicio 5.py
@@ -0,0 +1,17 @@
+'''Faça um programa em Python que recebe um número inteiro e imprime seu dígito das dezenas. Observe o exemplo abaixo:
+
+Entrada de Dados:
+Digite um número inteiro: 78615
+
+Saída de Dados:
+O dígito das dezenas é 1
+
+Entrada de Dados:
+Digite um número inteiro: 2
+
+Saída de Dados:
+O dígito das dezenas é 0
+
+Dica: O operador "//" faz uma divisão inteira jogando fora o resto, ou seja, aquilo que é menor que o divisor.
+O operador "%" devolve apenas o resto da divisão inteira jogando fora o resultado, ou seja,
+tudo que é maior ou igual ao divisor.'''
