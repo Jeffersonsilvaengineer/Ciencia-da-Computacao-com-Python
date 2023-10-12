986ef0d7f9209cc10cf937fb8d50ec346b7a4655
Jefferson Silva <jeffersonsilvaengineer@gmail.com>
2023-10-12 11:07:52 -0300

    Exercicios

--- Exercicio 4.py
+++ Exercicio 4.py
@@ -1,5 +1,4 @@
-'''Reescreva o programa contaSegundos para imprimir também a quantidade de dias, ou seja,
-faça um programa em Python que, dada a quantidade de segundos, "quebre" esse valor em dias, horas, minutos e segundos.
+'''Faça um programa em Python que, dada a quantidade de segundos, "quebre" esse valor em dias, horas, minutos e segundos.
 A saída deve estar no formato: a dias, b horas, c minutos e d segundos. Seja cuidadoso com o formato! Espaços a mais,
 vírgulas faltando ou outras diferenças são considerados erro
 
@@ -11,3 +10,12 @@ Por favor, entre com o número de segundos que deseja converter: 178615
 Saída de Dados:
 2 dias, 1 horas, 36 minutos e 55 segundos.'''
 
+seg = int(input('Digita a quantidade de segundos que deseja converter: '))
+a = seg // 86400  # 1 dia tem 86400 segundos
+seg %= 86400
+b = seg // 3600  # 1 hora tem 3600 segundos
+seg %= 3600
+c = seg // 60  # 1 minuto tem 60 segundos
+seg %= 60
+d = seg
+print(f'{a} dias, {b} horas, {c} minutos e {d} segundos.')
--- Exercicio 5.py
+++ Exercicio 5.py
@@ -15,3 +15,8 @@ O dígito das dezenas é 0
 Dica: O operador "//" faz uma divisão inteira jogando fora o resto, ou seja, aquilo que é menor que o divisor.
 O operador "%" devolve apenas o resto da divisão inteira jogando fora o resultado, ou seja,
 tudo que é maior ou igual ao divisor.'''
+
+x = int(input('Digite um número inteiro: '))
+dezena = x // 10
+dezena = dezena % 10
+print(dezena)
